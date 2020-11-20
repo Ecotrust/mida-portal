2b38d108c55907ace96be2472f896b50330d8a99
Ryan Hodges <ryan.d.hodges@gmail.com>
2020-11-19 17:22:07 -0800

    enabling overriding of templates, fixing font sizes in Ocean Stories

--- mida/settings.py
+++ mida/settings.py
@@ -1,3 +1,4 @@
+import os, pathlib
 # PROJECT_REGION = {
 #     'name': 'West Coast',
 #     'init_zoom': 5,
@@ -20,3 +21,25 @@ DATA_CATALOG_ENABLED = True
 # HANDLER_404 = 'wcoa.views.page_not_found'
 
 FEEDBACK_IFRAME_URL = "//docs.google.com/forms/d/e/1FAIpQLSfI6jIDyQzY1viSzEQHYGo4Z_yhFR49DhMpUIUTErBdkEvtxA/viewform?usp=sf_link"
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+APP_DIR = pathlib.Path(__file__).parent.absolute()
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(APP_DIR, 'templates')
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages'
+            ]
+        }
+    },
+]
