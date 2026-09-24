7b6acbbf3cd6ade753eb2ef835fc782fe28a6943
David Pollard <pollardld@gmail.com>
2026-09-24 16:35:18 -0700

    Add tests for URL legend rendering logic handling missing labels and URLs

--- mida/tests/test_ocean_story_content.py
+++ mida/tests/test_ocean_story_content.py
@@ -55,3 +55,32 @@ class OceanStoryContentLegendTest(SimpleTestCase):
             self.template[condition_start:row_condition],
         )
         self.assertIn("if (has_label || has_viz)", legend_row_code)
+
+    def test_url_legend_renders_unavailable_when_label_and_url_missing(self):
+        self.assertIn(
+            "if (entry.url === undefined && entry.label === undefined)",
+            self.template,
+        )
+        self.assertIn("<em>Legend not available</em>", self.template)
+
+    def test_url_legend_renders_image_when_label_missing(self):
+        self.assertIn("} else if (entry.label === undefined)", self.template)
+        self.assertIn(
+            'url + "/" + layer_id + "/images/" + entry.url',
+            self.template,
+        )
+
+    def test_url_legend_renders_label_when_url_missing(self):
+        self.assertIn("} else if (entry.url === undefined)", self.template)
+        self.assertIn('html += "<td>" + entry.label + "</td>";', self.template)
+
+    def test_url_legend_renders_image_and_label_when_both_present(self):
+        both_values_branch = self.template[self.template.index(
+            "} else {\n                                                                html += \"<tr valign='middle'>\";"
+        ):]
+
+        self.assertIn(
+            'html += "<td><img src=\\\"" + url + "/" + layer_id + "/images/" + entry.url + "\\\"></td>";',
+            both_values_branch,
+        )
+        self.assertIn('html += "<td>" + entry.label + "</td>";', both_values_branch)
